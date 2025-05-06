83aabc1b3f344b8d0c36e8b3637125f47942531a
Sean Li <15363540600@163.com>
2025-05-06 18:59:53 +1000

    fix: 🐛 Added correct margin to Sanity list contents

--- libs/sanity-toolkit/src/index.tsx
+++ libs/sanity-toolkit/src/index.tsx
@@ -1,8 +1,5 @@
 import imageUrlBuilder from '@sanity/image-url';
-import {
-  SanityAsset,
-  SanityImageObject,
-} from '@sanity/image-url/lib/types/types';
+import { SanityImageObject } from '@sanity/image-url/lib/types/types';
 import { PortableText, PortableTextProps } from 'next-sanity';
 import { FC } from 'react';
 import { client } from './client/client';
@@ -12,10 +9,6 @@ export * from './client/index';
 
 const builder = imageUrlBuilder(client);
 export const urlFor = (image: SanityImageObject) => builder.image(image);
-
-interface ImageAsset extends SanityAsset {
-  urlText: string;
-}
 
 export const SanityContent: FC<Pick<PortableTextProps, 'value'>> = ({
   value,
@@ -41,7 +34,9 @@ export const SanityContent: FC<Pick<PortableTextProps, 'value'>> = ({
       },
       list: {
         bullet: ({ children }) => (
-          <ul className="list-disc space-y-2 text-gray-600 pl-8">{children}</ul>
+          <ul className="list-disc space-y-2 text-gray-600 pl-8 mt-4">
+            {children}
+          </ul>
         ),
       },
     }}
